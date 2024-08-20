e31058c6fe7abcb96c8ef76cc56f56a1cdf0266d
prumat4 <didukhmaksym4@gmail.com>
2024-08-20 23:40:27 +0300

    simplify sniff_apckets

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,22 @@
+/*
+next steps:
+1. make the stats to save between reboots
+
+2. cli in separate thread :
+    add start one/all command
+    add stop one/all commnd
+    show ip count
+    show ip size
+    show stats for interface 
+    --help
+
+*/
+
+/*
+current problems:
+stats in general and indivudual jsons is different
+*/
+
 use crate::sniffer::{sniff_packets, PacketStats};
 use std::thread;
 use pnet::datalink;
@@ -26,46 +45,67 @@ impl Cli {
         }
     }
 
+    fn load_stats(&mut self) {
+        let mut json_dir = env::current_dir().expect("Failed to get current directory");
+        json_dir.push("jsons");
+    
+        if !json_dir.is_dir() {
+            println!("Jsons directory does not exist, creating new one");
+            std::fs::create_dir_all(&json_dir).expect("Failed to create jsons directory");
+        }
+    
+        let mut general_data_path = json_dir.clone();
+        general_data_path.push("general_data.json");
+        
+        if general_data_path.exists() {
+            let file = File::open(&general_data_path).expect("Failed to open general data file");
+            let data: HashMap<String, HashMap<IpAddr, PacketStats>> =
+                serde_json::from_reader(file).expect("Failed to parse general data file");
+            
+            self.data = data;
+            println!("Loaded general data from {}", general_data_path.display());
+        } else {
+            println!("No general data file found, starting with empty data");
+        }
+    }
+
     fn start_sniffing(&mut self) {
         let interfaces = datalink::interfaces();
         let (tx, rx) = mpsc::channel();
         let mut handles = vec![];
-
+    
         for interface in interfaces {
             let tx = tx.clone();
             let handle = thread::spawn(move || sniff_packets(interface, tx));
             handles.push(handle);
         }
-
+    
         drop(tx);
-
-        while let Ok((interface_name, stats)) = rx.recv() {
-            self.update_data(interface_name, stats);
-            // println!("Current aggregated data: {:#?}", self.data);
-
+    
+        while let Ok((interface_name, ip, packet_size)) = rx.recv() {
+            self.update_data(interface_name, ip, packet_size);
+    
             self.packets_since_last_save += 1;
-
-            if self.last_save_time.elapsed() >= Duration::new(10, 0) 
+    
+            if self.last_save_time.elapsed() >= Duration::new(1, 0) 
                 || self.packets_since_last_save >= self.packet_threshold {
                 self.save_stats();
                 self.last_save_time = Instant::now();
                 self.packets_since_last_save = 0;
             }
         }
-
+    
         for handle in handles {
             handle.join().unwrap();
         }
-    }
+    }    
 
-    fn update_data(&mut self, interface_name: String, stats: HashMap<IpAddr, PacketStats>) {
+    fn update_data(&mut self, interface_name: String, ip: IpAddr, packet_size: u64) {
         let entry = self.data.entry(interface_name.clone()).or_insert_with(HashMap::new);
+        let interface_entry = entry.entry(ip).or_insert(PacketStats { count: 0, size: 0 });
     
-        for (ip, new_stats) in stats {
-            let interface_entry = entry.entry(ip).or_insert(PacketStats { count: 0, size: 0 });
-            interface_entry.count = new_stats.count;
-            interface_entry.size = new_stats.size;
-        }
+        interface_entry.count += 1;
+        interface_entry.size += packet_size;
     }
 
     fn save_stats(&self) {
@@ -85,7 +125,8 @@ impl Cli {
                 println!("Saved stats for interface {} to {}", interface_name, file_path.display());
             }
         }
-    
+
+        // do we even need general stats? 
         let mut general_data_path = json_dir.clone();
         general_data_path.push("general_data.json");
         let file = File::create(&general_data_path).expect("Failed to create file");
@@ -98,20 +139,7 @@ impl Cli {
     }
     
     pub fn run(&mut self) {
+        self.load_stats();
         self.start_sniffing();
     }
 }
-
-/*
-next steps:
-1. make the stats to save between reboots
-
-2. cli in separate thread :
-    add start one/all command
-    add stop one/all commnd
-    show ip count
-    show ip size
-    show stats for interface 
-    --help
-
-*/
--- src/sniffer.rs
+++ src/sniffer.rs
@@ -6,9 +6,9 @@ use pnet::packet::Packet;
 use std::collections::HashMap;
 use std::net::IpAddr;
 use std::sync::mpsc;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PacketStats {
     pub count: u64,
     pub size: u64,
@@ -37,14 +37,12 @@ fn print_network_stats(interface_name: &str, stats: &HashMap<IpAddr, PacketStats
     println!("--------------------------------------------------------------------------------------");
 }
 
-pub fn sniff_packets(interface: NetworkInterface, tx: mpsc::Sender<(String, HashMap<IpAddr, PacketStats>)>) {
+pub fn sniff_packets(interface: NetworkInterface, tx: mpsc::Sender<(String, IpAddr, u64)>) {
     let mut rx = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(_, rx)) => rx,
         Ok(_) => panic!("Unhandled channel type for interface: {}", interface.name),
         Err(e) => panic!("Error creating datalink channel: {}", e),
     };
-
-    let mut network_stats: HashMap<IpAddr, PacketStats> = HashMap::new();
 
     println!("Started packet sniffing on: {}", interface.name);
 
@@ -52,33 +50,29 @@ pub fn sniff_packets(interface: NetworkInterface, tx: mpsc::Sender<(String, Hash
         match rx.next() {
             Ok(packet) => {
                 if let Some(ethernet_packet) = EthernetPacket::new(packet) {
-                    let mut updated = false;
-
                     match ethernet_packet.get_ethertype() {
                         EtherTypes::Ipv4 => {
                             if let Some(ipv4_packet) = Ipv4Packet::new(ethernet_packet.payload()) {
                                 let source_ip = IpAddr::V4(ipv4_packet.get_source());
                                 let packet_size = ipv4_packet.packet().len() as u64;
-                                update_stats(&mut network_stats, source_ip, packet_size);
-                                updated = true;
+
+                                if tx.send((interface.name.clone(), source_ip, packet_size)).is_err() {
+                                    println!("Failed to send packet stats");
+                                }
                             }
                         }
                         EtherTypes::Ipv6 => {
                             if let Some(ipv6_packet) = Ipv6Packet::new(ethernet_packet.payload()) {
                                 let source_ip = IpAddr::V6(ipv6_packet.get_source());
                                 let packet_size = ipv6_packet.packet().len() as u64;
-                                update_stats(&mut network_stats, source_ip, packet_size);
-                                updated = true;
+
+                                if tx.send((interface.name.clone(), source_ip, packet_size)).is_err() {
+                                    println!("Failed to send packet stats");
+                                }
                             }
                         }
                         _ => {
                             println!("Unhandled packet type: {:?}", ethernet_packet.get_ethertype());
-                        }
-                    }
-
-                    if updated {
-                        if tx.send((interface.name.clone(), network_stats.clone())).is_err() {
-                            println!("Failed to send packet stats");
                         }
                     }
                 }
